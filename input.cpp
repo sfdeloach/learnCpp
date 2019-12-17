4a70b0328f7c9bdcb36cee712dc61f56bb2e7484
Steven DeLoach <sfdeloach@gmail.com>
2019-12-17 18:21:28 -0500

    On branch master  Changes to be committed: 	modified:   input.cpp

--- input.cpp
+++ input.cpp
@@ -4,46 +4,47 @@
  * Simple program demonstrating the difference between cin and getline
  */
 
+using namespace std;
+
 int main()
 {
-   // std::cin acts like java's next, nextInt, nextDouble, etc. It will only store a token delimited
-   // by white space (new lines, tabs, and spaces).
-   //
-   // std::getline() acts like java's nextLine()
+    // std::cin acts like java's next, nextInt, nextDouble, etc. It will only
+    // store a token delimited by white space (new lines, tabs, and spaces).
+    //
+    // std::getline() acts like java's nextLine()
 
-   int i, j;
-   float f;
-   double d;
-   long l;
-   std::string s; // notice string type is in namespace std
+    int i, j;
+    float f;
+    double d;
+    long l;
+    string s; // notice string type is in namespace std
 
-   std::cout << "Enter two integers: ";
-   std::cin >> i >> j; // notice how two variables can be read by cin
-   std::cout << "You entered " << i << " and " << j << std::endl;
+    cout << "Enter two integers: ";
+    cin >> i >> j; // notice how two variables can be read by cin
+    cout << "You entered " << i << " and " << j << endl;
 
-   std::cout << "Enter a float: ";
-   std::cin >> f;
-   std::cout << "You entered " << f << std::endl;
+    cout << "Enter a float: ";
+    cin >> f;
+    cout << "You entered " << f << endl;
 
-   std::cout << "Enter a double: ";
-   std::cin >> d;
-   std::cout << "You entered " << d << std::endl;
+    cout << "Enter a double: ";
+    cin >> d;
+    cout << "You entered " << d << endl;
 
-   std::cout << "Enter a long: ";
-   std::cin >> l;
-   std::cout << "You entered " << l << std::endl;
+    cout << "Enter a long: ";
+    cin >> l;
+    cout << "You entered " << l << endl;
 
-   std::cout << "Enter a string (token): ";
-   std::cin >> s; // notice white spaces end the input
-   std::cout << "You entered \"" << s << "\"" << std::endl;
+    cout << "Enter a string (token): ";
+    cin >> s; // notice white spaces end the input
+    cout << "You entered \"" << s << "\"" << endl;
 
-   // clear keyboard buffer
-   std::getline(std::cin, s);
+    // clear keyboard buffer
+    getline(cin, s);
 
-   std::cout << "Enter a string (line): ";
-   std::getline(std::cin, s);
-   std::cout << "You entered \"" << s << "\"" << std::endl;
+    cout << "Enter a string (line): ";
+    getline(cin, s);
+    cout << "You entered \"" << s << "\"" << endl;
 
-   return 0;
+    return 0;
 }
- 
