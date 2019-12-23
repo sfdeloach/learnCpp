fac9592d6eda4ae30d0a2b8b6727199b9b67c678
Steven DeLoach <sfdeloach@gmail.com>
2019-12-22 21:33:13 -0500

    On branch master  Changes to be committed: 	deleted:    05/run.sh 	new file:   07/69-multiTable.cpp 	renamed:    06/run.sh -> 07/run.sh 	new file:   run.sh

--- 05/run.sh
+++
@@ -1,6 +0,0 @@
-# compile with optimization, warning flags, and C++17 standard
-g++ -O3 -Wall -std=c++17 $1
-
-# run the program
-./a.out
-
---
+++ 07/69-multiTable.cpp
@@ -0,0 +1,40 @@
+/*
+ *  title:       Multiplication Table
+ *  author:      Steven DeLoach (sfdeloach@gmail.com)
+ *  date:        Dec 22, 2019
+ *  description: Create a multiplication table using for loops
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int columns, rows;
+
+   cout << "Enter columns: ";
+   cin >> columns;
+   cout << "Enter rows: ";
+   cin >> rows;
+
+   // print column header
+   cout << "\t";
+   for (int i = 0; i <= columns; ++i)
+   {
+      cout << i << "\t";
+   }
+   cout << endl;
+
+   for (int i = 0; i <= columns; ++i)
+   {
+      cout << i << "\t";
+      for (int j = 0; j <= rows; ++j)
+      {
+         cout << (i * j) << "\t";
+      }
+      cout << endl;
+   }
+
+   return 0;
+}
